02f230532d6ff7402d39e714d516832ce90e7cc7
Tom <tom.loustau@gmail.com>
2025-11-10 10:08:00 +0100

    Ajout de tout les attributs de la classe Annonce

--- modeles/annonce.class.php
+++ modeles/annonce.class.php
@@ -2,4 +2,14 @@
 
 require_once "include.php"; 
 
+class Annonce{
+    private ?string $id;
+    private ?string $titre;
+    private ?string $description;
+    private ?int $etat; // int qui correspond à un état
+    private ?string $datePublication;
+    private ?string $dateDebut;
+    private ?string $dateFin;
+}
+
 ?>
