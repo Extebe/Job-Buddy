f2da94f7ec14439dfefcdf01d7c74e9e44989c60
Tom <tom.loustau@gmail.com>
2025-11-10 11:00:28 +0100

    Création de la classe annonceDao (pas finit)

--- modeles/annonce.dao.php
+++ modeles/annonce.dao.php
@@ -1,3 +1,37 @@
 <?php
-
 require_once "include.php";
+
+
+class AnnonceDao{
+    private ?PDO $pdo;
+
+    public function __construct(?PDO $pdo=null){
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * Get the value of pdo
+     */
+    public function getPdo(): ?PDO
+    {
+        return $this->pdo;
+    }
+
+    /**
+     * Set the value of pdo
+     */
+    public function setPdo(?PDO $pdo): void
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function find(?int $id): ?Annonce
+    {
+        $sql = "SELECT * FROM annonce";
+        $pdoStatement = $this->pdo->prepare($sql);
+        $pdoStatement->execute();
+        $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Annonce');
+        $annonce = $pdoStatement->fetch();
+        return $annonce;
+    }
+}
