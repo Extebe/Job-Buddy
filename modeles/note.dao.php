6872a2dcafa915006ce1407d5a33a95c1a018208
Extebe <eextebe@gmail.com>
2025-11-12 16:19:46 +0100

    Ajout du dao de la classe note

--- modeles/note.dao.php
+++ modeles/note.dao.php
@@ -1,3 +1,31 @@
 <?php
 
 require_once "include.php";
+
+
+class NoteDao{
+    private ?PDO $pdo;
+
+    public function __construct(?PDO $pdo=null){
+        $this->pdo = $pdo;
+    }
+    
+    public function findAll
+
+
+    public function findByUser(string $idUtilisateur): array{
+        //requete
+        $sql = "SELECT * FROM Note where ";
+        $pdoStatement = $this->pdo->prepare($sql);
+        $pdoStatement->execute();
+        $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+        $uneNote = $pdoStatement->fetch();
+
+    }
+
+    public function hydrate (array $tableau): Note{
+        $note = new Note();
+        $setId=
+    }
+    
+}
