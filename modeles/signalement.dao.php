9a4ac95509915c3bad4d5b2e4aed4f2819f0786f
Steven Doree Dinh <sddinh@iutbayonne.univ-pau.fr>
2025-11-12 12:24:48 +0100

    Ajout de la classe signalementDao

--- modeles/signalement.dao.php
+++ modeles/signalement.dao.php
@@ -1,3 +1,29 @@
 <?php
 
-require_once "include.php";
+    require_once "include.php";
+    class SignalementDao {
+        private ?PDO $pdo;
+
+        public function __construct(?PDO $pdo = null){
+            $this->pdo = $pdo;
+        }
+
+        /**
+         * Get the value of pdo
+         */ 
+        public function getPdo():?PDO
+        {
+                return $this->pdo;
+        }
+
+        /**
+         * Set the value of pdo
+         *
+         */ 
+        public function setPdo($pdo):void
+        {
+                $this->pdo = $pdo;
+;
+        }
+    }
+?>
