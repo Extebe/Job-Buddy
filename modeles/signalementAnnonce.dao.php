701fb4d856109deafa8fe559b4c36b96cf0bb48b
Steven Doree Dinh <sddinh@iutbayonne.univ-pau.fr>
2025-11-14 12:19:20 +0100

    Ajout des classes dao de signalement annonce et utilisateur

--- modeles/signalementAnnonce.dao.php
+++ modeles/signalementAnnonce.dao.php
@@ -1,3 +1,56 @@
 <?php
 
-require_once "include.php";
+    require_once "include.php";
+    class SignalementAnnonceDoa extends Signalement{
+        public function findAll(){
+            $sql="SELECT * FROM signalement WHERE idUtilisateurSignale IS NULL AND idAnnonceSignale IS NOT NULL";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute();
+            $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"signalement");
+            
+            $signalement=$pdoStatement->fetchAll();
+            return $signalement;
+        }
+        public function find(?int $id):?SignalementAnnonce{
+            $sql="SELECT * FROM signalement WHERE idUtilisateurSignale IS NULL AND idAnnonceSignale = :id";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute(array("id"=>$id));
+            $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"signalement");
+            
+            $signalement=$pdoStatement->fetch();
+            return $signalement;
+        }
+        public function findAllAssoc(){
+            $sql="SELECT * FROM signalement WHERE idUtilisateurSignale IS NULL AND idAnnonceSignale IS NOT NULL";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute();
+            $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+            
+            $signalement=$pdoStatement->fetchAll();
+            return $signalement;
+        }
+        public function findAssoc(?int $id):?SignalementAnnonce{
+            $sql="SELECT * FROM signalement WHERE idUtilisateurSignale IS NULL AND idAnnonceSignale = :id";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute(array("id"=>$id));
+            $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+            
+            $signalement=$pdoStatement->fetch();
+            return $signalement;
+        }
+
+        public function hydrate($tabAssoc): ?SignalementAnnonce{
+            $signalementAnnonce=new SignalementAnnonce($tabAssoc['signaleur'],$tabAssoc['dateSignalement'],$tabAssoc['motif'],$tabAssoc['description'],$tabAssoc['annondeSignale'],);
+            return $signalementAnnonce;
+        }
+
+        public function hydrateAll($tab):?array{
+            $listeSignalementAnnonce=[];
+            foreach ($tab as $tabAssoc) {
+                $signalementAnnonce=$this->hydrate($tabAssoc);
+                $listeSignalementAnnonce[]=$signalementAnnonce;
+            }
+            return $listeSignalementAnnonce;
+        }
+    }
+?>
--- modeles/signalementUtilisateur.dao.php
+++ modeles/signalementUtilisateur.dao.php
@@ -1,3 +1,57 @@
 <?php
 
-require_once "include.php";
+    require_once "include.php";
+    class SignalementUtilisateurDao extends Signalement{
+        public function findAll(){
+            $sql="SELECT * FROM signalement WHERE idAnnonceSignale IS NULL AND idUtilisateurSignale IS NOT NULL";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute();
+            $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"signalement");
+            
+            $signalement=$pdoStatement->fetchAll();
+            return $signalement;
+        }
+        public function find(?int $id):SignalementUtilisateur{
+            $sql="SELECT * FROM signalement WHERE idAnnonceSignale IS NULL AND idUtilisateurSignale =:id";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute(array("id"=>$id));
+            $pdoStatement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE,"signalement");
+            
+            $signalement=$pdoStatement->fetch();
+            return $signalement;
+        }
+        public function findAllAssoc(){
+            $sql="SELECT * FROM signalement WHERE idAnnonceSignale IS NULL AND idUtilisateurSignale IS NOT NULL";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute();
+            $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+            
+            $signalement=$pdoStatement->fetchAll();
+            return $signalement;
+        }
+        public function findAssoc(?int $id):SignalementUtilisateur{
+            $sql="SELECT * FROM signalement WHERE idAnnonceSignale IS NULL AND idUtilisateurSignale =:id";
+            $pdoStatement=$this->getPdo()->prepare($sql);
+            $pdoStatement->execute(array("id"=>$id));
+            $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+            
+            $signalement=$pdoStatement->fetch();
+            return $signalement;
+        }
+
+        public function hydrate($tableauAssoc):?SignalementUtilisateur{
+            $signalementUtilisateur = new SignalementUtilisateur($tableauAssoc['signaleur'],$tableauAssoc['dateSignalement'],$tableauAssoc['motif'],$tableauAssoc['description'],$tableauAssoc['utilisateurSignale']);
+            return $signalementUtilisateur;
+        }
+        public function hydrateAll($tab): ?array{
+            $listeUtilisateurSignale=[];
+            foreach($tab as $tabAssoc){
+                $utilisateur=$this->hydrate($tabAssoc);
+                $listeUtilisateurSignale[]=$utilisateur;
+            }
+            return $listeUtilisateurSignale;
+        }
+        
+    }
+    
+?>
