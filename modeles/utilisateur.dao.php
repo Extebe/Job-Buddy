3c02b8c7774996c89640a448a9ebc21caa529556
Tom <tom.loustau@gmail.com>
2025-12-15 13:07:11 +0100

    ajout de la méthode inserUtilisateur car disparition improbable

--- modeles/utilisateur.dao.php
+++ modeles/utilisateur.dao.php
@@ -24,5 +24,25 @@ class UtilisateurDao{
     {
         $this->pdo = $pdo;
     }
+
+    public function insererUtilisateur($user, $passwordHache): void {
+        $requete = "INSERT INTO Utilisateur (role, codeINE, nom, prenom, tel, dateNaiss, email, mdp, ville, adresse, codePostal) 
+        values (:role, :codeINE, :nom, :prenom, :tel, :dateNaiss, :email, :mdp, :ville, :adresse, :codePostal);";
+        $pdoStatement = $this->pdo->prepare($requete);
+        $pdoStatement->execute([
+            ':role' => $user->getRole(),
+            ':codeINE' => $user->getCodeINE(),
+            ':nom' => $user->getNom(),
+            ':prenom' => $user->getPrenom(),
+            ':tel' => $user->getTelephone(),
+            ':dateNaiss' => $user->getDateNaiss(),
+            ':email' => $user->getEmail(),
+            ':mdp' => $user->getMdp(),
+            ':ville' => $user->getVille(),
+            ':adresse' => $user->getAdresse(),
+            ':codePostal' => $user->getCodePostal()
+        ]);
+    }
+
 }
 ?>
