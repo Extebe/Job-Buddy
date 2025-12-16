43594dd68628737c30e082440de8f5fe0db49005
Steven Doree Dinh <sddinh@iutbayonne.univ-pau.fr>
2025-12-16 09:00:39 +0100

    Correction d'une erreur sur la connexion

--- controllers/controller.utilisateur.php
+++ controllers/controller.utilisateur.php
@@ -53,8 +53,8 @@ class ControllerUtilisateur extends Controller
 
     /*==============================
      *
-     *  Pous se connecter à la page 
-     *  de connexion
+     *  
+     *   
      * 
      ===============================*/
     public function inscriptionBd(Utilisateur $user){
@@ -145,7 +145,7 @@ class ControllerUtilisateur extends Controller
 
         // Recherche de l'utilisateur
         $requete= $pdo->prepare(
-            'SELECT id, mdp, role FROM utilisateur WHERE email =:email'
+            'SELECT id, mdp, role FROM Utilisateur WHERE email =:email'
         );
 
         // Exécution de la requête avec l'email de l'utilisateur
@@ -165,6 +165,7 @@ class ControllerUtilisateur extends Controller
                 $_SESSION['role'] = $donneeUtilisateurEnBD['role'];
                 return true; // Authentification réussie
             }
+            throw new Exception("mdp_invalide");
         }
         return false; // Authentification échouée
     }
@@ -189,15 +190,12 @@ class ControllerUtilisateur extends Controller
                     echo "Connexion réussie.";
                     echo "<br><a href='index.php'>Retourner à l'accueil</a>";
                 }
-                else{
-                    echo "Erreur : Email ou mot de passe incorrect.";
-                    echo '<br><a href="index.php?controleur=utilisateur&methode=pageConnexion">Retourner à la page de connexion</a>';
-                }
-                return true;
             }
             catch (Exception $e){
                 switch($e ->getMessage()){
-                    case "email_ou_mdp_incorrect":
+                    case "mdp_invalide":
+                        echo "L'email ou le mot de passe est incorrect<br>";
+                        echo "<a href='index.php?controleur=utilisateur&methode=pageConnexion'>Retour à la page de connexion</a><br>";
 
                 }
             }
