2f65b9e6c55452b8e44f5df6c6151337f89c3a80
Nilo <nilo.duvignau@gmail.com>
2026-01-22 11:56:29 +0100

    Maj de la DBB (table signalement)

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-/*
+-- Suppression des tables :
 DROP TABLE IF EXISTS SignalementAnonce;
 DROP TABLE IF EXISTS SignalementUtilisateur;
 DROP TABLE IF EXISTS Note;
@@ -7,114 +7,111 @@ DROP TABLE IF EXISTS Signalement;
 DROP TABLE IF EXISTS Annonce;
 DROP TABLE IF EXISTS Utilisateur;
 DROP TABLE IF EXISTS InscritNewsLetter;
-*/
 
 
+-- Création des tables :
 CREATE TABLE Utilisateur(
-   id int AUTO_INCREMENT,
-   role VARCHAR(14) NOT NULL CHECK (role IN ('PARTICULIER', 'ETUDIANT', 'ADMINISTRATEUR')),
-   codeINE VARCHAR(20),
-   nom VARCHAR(50) NOT NULL,
-   prenom VARCHAR(50) NOT NULL,
-   tel CHAR(10),
-   dateNaiss DATE NOT NULL,
-   email VARCHAR(50) NOT NULL,
-   mdp VARCHAR(250) NOT NULL,
-   dateSuppression DATE,
-   ville VARCHAR(50),
-   adresse VARCHAR(50),
-   codePostal VARCHAR(5),
-   tentativesEchouees INT DEFAULT 0 NOT NULL,
-   dateDernierEchecConnexion DATETIME DEFAULT NULL, -- Date et heure du dernier échec de connexion
-   statutCompte ENUM('actif', 'desactive') DEFAULT 'actif',
-   cvec VARCHAR(12),
-   PRIMARY KEY(id)
+  id int AUTO_INCREMENT,
+  role VARCHAR(14) NOT NULL CHECK (role IN ('PARTICULIER', 'ETUDIANT', 'ADMINISTRATEUR')),
+  codeINE VARCHAR(20),
+  nom VARCHAR(50) NOT NULL,
+  prenom VARCHAR(50) NOT NULL,
+  tel CHAR(10),
+  dateNaiss DATE NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  mdp VARCHAR(250) NOT NULL,
+  dateSuppression DATE,
+  ville VARCHAR(50),
+  adresse VARCHAR(50),
+  codePostal VARCHAR(5),
+  tentativesEchouees INT DEFAULT 0 NOT NULL,
+  dateDernierEchecConnexion DATETIME DEFAULT NULL, -- Date et heure du dernier échec de connexion
+  statutCompte ENUM('actif', 'desactive') DEFAULT 'actif',
+  cvec VARCHAR(12),
+  PRIMARY KEY(id)
 );
+
 
 CREATE TABLE Annonce(
-   id int AUTO_INCREMENT,
-   idParticulier int NOT NULL,
+  id int AUTO_INCREMENT,
+  idParticulier int NOT NULL,
 
-   titre VARCHAR(100),
-   description VARCHAR(1000),
-   typeService VARCHAR(25) CHECK (typeService IN ('baby-sitting', 'jardinage', 'bricolage','ménage', 'transport', 'aide informatique', 'aide aux devoirs', 'autre')),
-   lieu VARCHAR(100),
-   remuneration DECIMAL(6,2) CHECK (remuneration >= 0),
 
-   dateDebutRealisation DATETIME,
-   dateFinRealisation DATETIME CHECK (dateFinRealisation > dateDebutRealisation),
+  titre VARCHAR(100),
+  description VARCHAR(1000),
+  typeService VARCHAR(25) CHECK (typeService IN ('baby-sitting', 'jardinage', 'bricolage','ménage', 'transport', 'aide informatique', 'aide aux devoirs', 'autre')),
+  lieu VARCHAR(100),
+  remuneration DECIMAL(6,2) CHECK (remuneration >= 0),
 
-   etat VARCHAR(20) NOT NULL CHECK (etat IN ('DISPONIBLE','ACCEPTE','TERMINE')),
 
-   datePublication DATETIME,
-   dateSuppression DATETIME,
-   motifSuppression VARCHAR(50),
+  dateDebutRealisation DATETIME,
+  dateFinRealisation DATETIME CHECK (dateFinRealisation > dateDebutRealisation),
 
-   PRIMARY KEY(id),
-   FOREIGN KEY(idParticulier) REFERENCES Utilisateur(id)
+
+  etat VARCHAR(20) NOT NULL CHECK (etat IN ('DISPONIBLE','ACCEPTE','TERMINE')),
+
+
+  datePublication DATETIME,
+  dateSuppression DATETIME,
+  motifSuppression VARCHAR(50),
+
+
+  PRIMARY KEY(id),
+  FOREIGN KEY(idParticulier) REFERENCES Utilisateur(id)
 );
+
+
 
 
 CREATE TABLE Signalement(
-   id int AUTO_INCREMENT,
-   dateSignalement DATETIME NOT NULL,
-   motif VARCHAR(20),
-   description VARCHAR(500),
-   idSignaleur int NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(idSignaleur) REFERENCES Utilisateur(id)
-   );
+  id int AUTO_INCREMENT,
+  dateSignalement DATETIME NOT NULL,
+  motif VARCHAR(20),
+  description VARCHAR(500),
+  idSignaleur int NOT NULL,
+  idUtilisateurSignale int,
+  idAnnonceSignale int,
+  PRIMARY KEY(id),
+  FOREIGN KEY(idSignaleur) REFERENCES Utilisateur(id),
+  FOREIGN KEY(idUtilisateurSignale) REFERENCES Utilisateur(id),
+  FOREIGN KEY(idAnnonceSignale) REFERENCES Annonce(id)
+  );
 
-CREATE TABLE SignalementUtilisateur(
-   idSignalement int,
-   idUtilisateurSignale int,
-   FOREIGN KEY(idSignalement) REFERENCES Signalement(id),
-   FOREIGN KEY(idUtilisateurSignale) REFERENCES Utilisateur(id),
-   PRIMARY KEY(idSignalement,idUtilisateurSignale)
-);
-
-CREATE TABLE SignalementAnonce(
-   idSignalement int,
-   idAnnonceSignale int,
-   FOREIGN KEY(idSignalement) REFERENCES Signalement(id),
-   FOREIGN KEY(idAnnonceSignale) REFERENCES Annonce(id),
-   PRIMARY KEY(idSignalement,idAnnonceSignale)
-);
 
 CREATE TABLE Postuler(
-   idAnnonce int,
-   idEtudiant int,
-   datePostulat DATETIME NOT NULL,
-   estAccepte BOOLEAN NOT NULL,
-   PRIMARY KEY(idAnnonce, idEtudiant),
-   FOREIGN KEY(idAnnonce) REFERENCES Annonce(id),
-   FOREIGN KEY(idEtudiant) REFERENCES Utilisateur(id)
+  idAnnonce int,
+  idEtudiant int,
+  datePostulat DATETIME NOT NULL,
+  estAccepte BOOLEAN NOT NULL,
+  PRIMARY KEY(idAnnonce, idEtudiant),
+  FOREIGN KEY(idAnnonce) REFERENCES Annonce(id),
+  FOREIGN KEY(idEtudiant) REFERENCES Utilisateur(id)
 );
+
 
 CREATE TABLE Note(
-   id int AUTO_INCREMENT,
-   idAnnonce int NOT NULL,
-   idUtilisateurNoteur int NOT NULL,
-   idUtilisateurNote int NOT NULL,
-   note SMALLINT NOT NULL CHECK (note >= 0 AND note <= 5),
-   commentaire VARCHAR(100),
-   PRIMARY KEY(id),
-   FOREIGN KEY(idAnnonce) REFERENCES Annonce(id),
-   FOREIGN KEY(idUtilisateurNoteur) REFERENCES Utilisateur(id),
-   FOREIGN KEY(idUtilisateurNote) REFERENCES Utilisateur(id)
+  id int AUTO_INCREMENT,
+  idAnnonce int NOT NULL,
+  idUtilisateurNoteur int NOT NULL,
+  idUtilisateurNote int NOT NULL,
+  note SMALLINT NOT NULL CHECK (note >= 0 AND note <= 5),
+  commentaire VARCHAR(100),
+  PRIMARY KEY(id),
+  FOREIGN KEY(idAnnonce) REFERENCES Annonce(id),
+  FOREIGN KEY(idUtilisateurNoteur) REFERENCES Utilisateur(id),
+  FOREIGN KEY(idUtilisateurNote) REFERENCES Utilisateur(id)
 );
+
 
 CREATE TABLE InscritNewsLetter(
-   id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
-   email varchar(50)
+  id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  email varchar(50)
 );
 
-
-
-
+-- INSERTIONS 
 INSERT INTO Utilisateur VALUES(1, 'PARTICULIER', '123456789012', 'Dupont', 'Jean', '0123456789', '1990-05-15', 'jean.dupont@example.com', 'mdp123', NULL, 'Paris', '10 rue de Paris', '75001', 0, NULL, 'actif',NULL);
 INSERT INTO Utilisateur VALUES(2, 'ETUDIANT', '123456789013', 'Martin', 'Sophie', '0123456790', '2000-10-25', 'sophie.martin@example.com', 'mdp456', NULL, 'Lyon', '20 rue de Lyon', '69001', 0, NULL, 'actif',NULL);
-INSERT INTO Utilisateur VALUES(3, 'ADMINISTRATEUR', '123456789014', 'admin', 'admin', '0123456791', '1985-03-10', 'admin@example.com', '', NULL, 'Marseille', '30 rue de Marseille', '13001', 0, NULL, 'actif',NULL);
+
 
 INSERT INTO Annonce VALUES (1, 1, "Garde d'enfant", "Garde d'enfant pour une journée complète.", 'baby-sitting', 'Paris', 100.00, '2025-11-01 10:00:00', '2025-11-10 18:00:00', 'DISPONIBLE', '2025-11-01 10:00:00', NULL, NULL);
 INSERT INTO Annonce VALUES (2, 1, 'Réparation de plomberie', "Réparation d'une fuite d'eau.", 'bricolage', 'Lyon', 80.00, '2025-11-02 09:00:00', '2025-11-05 17:00:00', 'ACCEPTE', '2025-11-02 09:00:00', NULL, NULL);
@@ -133,17 +130,16 @@ INSERT INTO Annonce VALUES (14, 1, 'Cours initiation informatique', 'Initiation 
 INSERT INTO Annonce VALUES (15, 1, 'Petit bricolage maison', 'Réparation de poignées et remplacement d’ampoules.', 'bricolage', 'Paris', 55.00, '2025-12-20 10:00:00', '2025-12-20 12:00:00', 'DISPONIBLE', '2025-11-17 15:00:00', NULL, NULL);
 
 
-INSERT INTO Signalement VALUES(1, '2025-11-03 14:30:00', 'Contenu inapproprié', "Annonce de garde d'enfant avec des détails douteux.", 2);
-INSERT INTO Signalement VALUES(2, '2025-11-04 11:00:00', 'Faux service', 'Annonce de bricolage pour un travail qui semble être une fraude.', 3);
+INSERT INTO Signalement VALUES(1, '2025-11-03 14:30:00', 'Contenu inapproprié', "Annonce de garde d'enfant avec des détails douteux.", 2, NULL, 1);
+INSERT INTO Signalement VALUES(2, '2025-11-04 11:00:00', 'Faux service', 'Annonce de bricolage pour un travail qui semble être une fraude.', 1, NULL, 2);
+INSERT INTO Signalement VALUES (3, '2025-11-06 16:45:00', 'Comportement', 'L’étudiant ne s’est pas présenté à la mission acceptée.', 1, 2, NULL);
+INSERT INTO Signalement VALUES (4, '2025-11-07 09:20:00', 'Paiement', 'Le particulier n’a pas payé la mission après réalisation.', 2, 1, NULL);
 
-INSERT INTO SignalementUtilisateur VALUES(1, 3);
-INSERT INTO SignalementUtilisateur VALUES(2, 1);
-
-INSERT INTO SignalementAnonce VALUES(1, 1);
-INSERT INTO SignalementAnonce VALUES(2, 2);
 
 INSERT INTO Postuler VALUES(1, 2, '2025-11-03 14:00:00', TRUE);
-INSERT INTO Postuler VALUES(2, 3, '2025-11-04 10:00:00', FALSE);
+INSERT INTO Postuler VALUES(2, 2, '2025-11-04 10:00:00', FALSE);
+
 
 INSERT INTO Note VALUES(1, 1, 1, 2, 4, 'Très bonne expérience, mais un peu trop cher.');
-INSERT INTO Note VALUES(2, 2, 3, 1, 2, "Le travail n'a pas été bien fait.");
+INSERT INTO Note VALUES(2, 2, 2, 1, 2, "Le travail n'a pas été bien fait.");
+
