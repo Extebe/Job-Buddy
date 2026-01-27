451ba56590b6fb7d175935e0d1941b3f6a65cf15
Tom <tom.loustau@gmail.com>
2026-01-27 10:37:18 +0100

    correction du probleme lors de l'ajout des annonces

--- controllers/controller.annonce.php
+++ controllers/controller.annonce.php
@@ -90,16 +90,16 @@ class ControllerAnnonce extends Controller
                 null
             );
             $managerAnnonce = new AnnonceDAO($this->getPdo());
+            $managerAnnonce->insererAnnonce($annonce1);
             try {
                 if (!$particulier) {
                     throw new Exception("Erreur : Impossible de récupérer le profil Particulier. Verifiez que vous tes connecté avec un compte Particulier.");
                 }
-                $managerAnnonce->insererAnnonce($annonce1);
             } catch (\Exception $e) {
                 echo "Erreur lors de l'insertion : " . $e->getMessage();
             }
             //Appel de la requête qui créé l'annonce
-            header("Location: index.php?controleur=annonce&methode=afficherMesAnnonces&filtre=ALL");
+            header("Location: index.php?controleur=annonce&methode=afficherMesAnnonces&filtre=all");
             exit();
 
         }
